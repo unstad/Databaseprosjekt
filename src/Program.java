6bc8cd1765710d7bf408e62cfa8adb690f2f06d1
gwib <galinavj@stud.ntnu.no>
2016-03-17 11:33:19 +0100

    lagt inn variablene paa toppen, fisket smaafeil

--- src/Program.java
+++ src/Program.java
@@ -1,40 +1,48 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.sql.Statement;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Program{
+public class Program extends Application{
     LocalDateTime dato;
-    Scanner scan = new Scanner();
-    String navn, valg;
+    Scanner scan = new Scanner(System.in);
+    String navn, valg, maal, resultat, notat, luftkvalitet, vaer;
+    ArrayList muskelgrupper;
+    LagAktivitet aktivitet;
+    boolean inne;
+    int prestasjon, varighet;
 
-    public void main(){
+    public void main() {
         System.out.println("Hei! Velkommen til treningsdagboken din");
         System.out.println("Skriv 1 hvis du vil opprette en ny aktivitet");
         System.out.println("Skriv 2 hvis du vil registrere et resultat");
         System.out.println("Skriv 3 hvis du vil registrere en treningsøkt");
         System.out.println("Skriv 4 hvis du vil hente ut en aktivitet");
         System.out.println("Skriv 0 hvis du vil avslutte");
-        valg = scan.NextInt
-        while(valg==1 || valg==2 || valg==3){
-            if(valg==1){
+        valg = scan.nextLine();
+        while(valg=="1" || valg=="2" || valg=="3"){
+            if(valg=="1"){
                 System.out.println("Hva vil du kalle aktiviteten?");
                 navn = scan.nextLine();
-                Aktivitet navn = new Aktivitet(navn,beskrivelse,maal,resultat, muskelgrupper);
-                System.out.println("Hva vil du kalle aktiviteten?");
-                navn.navn = scan.NextLine();
                 System.out.println("Skriv inn beskrivelse for aktiviteten:");
-                navn.beskrivelse = scan.nextLine();
+                String beskrivelse = scan.nextLine();
                 System.out.println("Hva er målet for aktiviteten?");
-                navn.maal = scan.nextLine();
+                maal = scan.nextLine();
                 System.out.println("Hva er resultatet?");
-                navn.resultat = scan.nextLine();
+                resultat = scan.nextLine();
                 System.out.println("Hvilke(n) muskelgruppe(r)aktiveres? Tast 0 for å avslutte");
-               while (scan.NextLine != '0'){
-                   Array muskelgrupper = new Array[];
-                   muskelgrupper.add(scan.NextLine);
+               while (scan.nextLine() != "0"){
+
+                   muskelgrupper.add(scan.nextLine());
                }
-                navn.muskelgrupper = muskelgrupper;
+                aktivitet = LagAktivitet(navn, beskrivelse, maal,resultat, muskelgrupper);
+                //Why??
             }
-            else if(valg==2){
+            //må knyttes til aktivitet, vet ikke hvordan vi skal implementere dette
+            else if(valg=="2"){
                 /*Resultat result = new resultat(resultatID,oektID,aktivitetNavn));
                 System.out.println("Er resultatet for kondisjon eller en styrkeøvelse? Tast 1 for kondisjon, 2 for styrkeøvelse");
                 if(scan.NextInt == 1){
@@ -48,39 +56,45 @@ public class Program{
                     }
                 }*/
             }
-            else if(valg==3){
+            else if(valg=="3"){
                 System.out.println("Hva vil du kalle treningen?");
                 navn = scan.nextLine();
-                Treningsoekt navn = new Treningsoekt(notat,ID,luftkvalitet,inne,vaer,prestasjon,varighet);
+
                 System.out.println("Skriv inn notat for treningen:");
-                navn.notat = scan.nextLine();
+                notat = scan.nextLine();
                 System.out.println("Hvordan var luftkvaliteten?");
-                navn.luftkvalitet = scan.nextLine();
+                luftkvalitet = scan.nextLine();
                 System.out.println("Hvordan var været?");
-                navn.vaer = scan.nextLine();
+                vaer = scan.nextLine();
                 System.out.println("Skriv 1 hvis treningen var inne, skriv 0 hvis treningen var ute");
                 while(scan.nextInt()!=1 || scan.nextInt()!=0){
                     System.out.println("Velg enten 1 eller 0");
                     System.out.println("Skriv 1 hvis treningen var inne, skriv 0 hvis treningen var ute");
                 }
                 if(scan.nextInt()==1){
-                    navn.inne = true;
+                    inne = true;
                 }
                 else if(scan.nextInt()==0) {
-                    navn.inne = false;
+                    inne = false;
                 }
                 System.out.println("Ranger prestasjonen din fra 0-9");
-                navn.prestasjon = scan.nextInt();
+                prestasjon = scan.nextInt();
                 System.out.println("Hvor lenge varte treningen?[min]");
-                navn.varighet = scan.nextLine();
+                varighet = scan.nextInt();
+                Treningsoekt navn = new Treningsoekt(notat,dato,luftkvalitet,inne,vaer,prestasjon,varighet);
             }
-            else if(valg ==4){
+            else if(valg =="4"){
                 System.out.println("Hvilken vil du hente ut?");
-                navn = scan.NextLine;
+                navn = scan.nextLine();
+                //??
                 Statement stmt = conn.createStatement();
-                stmt.executeQuery("select * from aktivitet where navn = navn"); //TODO
+                stmt.executeQuery("select * from aktivitet where navn == 'navn'"); //TODO
             }
         }
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
